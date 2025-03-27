6e50d3f3571ab1089e8173df325e66f23d2e8d0a
Тури Фурри <159616423+K2254IVV@users.noreply.github.com>
2025-03-27 05:16:53 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,38 @@
+mkdir WinOS12
+cd WinOS12
+git clone https://github.com/yeyushengfan258/Win12OS-kde
+cd Win12OS-kde
+./install.sh
+cd ..
+cd ..
+mkdir WinOS10
+cd WinOS10
+git clone https://github.com/yeyushengfan258/We10XOS-kde
+cd We10XOS-kde
+./install.sh
+cd ..
+cd ..
+mkdir WinOS11
+cd WinOS11
+git clone https://github.com/yeyushengfan258/Win11OS-kde
+cd Win11OS-kde
+./install.sh
+cd ..
+cd ..
+mkdir WinOSXi
+cd WinOSXi
+git clone https://github.com/yeyushengfan258/We10X-icon-theme
+cd We10X-icon-theme
+./install.sh
+cd ..
+cd ..
+mkdir WinOSXIi
+cd WinOSXIi
+git clone https://github.com/yeyushengfan258/Win11-icon-theme
+cd Win11-icon-theme
+./install.sh
+cd ..
+cd ..
+echo T4W package installed!
+echo Rebooting...
+reboot
